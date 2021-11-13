c3ede344641b754aef35c092e616a2e594a4785f
Arturov Kirill <kaarturov@sberbank.ru>
2021-11-13 20:00:55 +0300

    Updates

---
+++ precache-manifest.b24e2fe16ca59210833f14fbef7544ca.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09b738bd8fa00aafa68f0431ecc62908",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ff45aecef15885042ad",
+    "url": "/static/css/main.a2689c4e.chunk.css"
+  },
+  {
+    "revision": "8fc3b7f804ed30727ba9",
+    "url": "/static/js/2.ccacdf0a.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/static/js/2.ccacdf0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ff45aecef15885042ad",
+    "url": "/static/js/main.fce4ffeb.chunk.js"
+  },
+  {
+    "revision": "84f2f1592e1fc5da9888",
+    "url": "/static/js/runtime-main.48ea7090.js"
+  },
+  {
+    "revision": "f2a01c3dec0eb7365f7e1adba32ae831",
+    "url": "/static/media/cross.f2a01c3d.svg"
+  },
+  {
+    "revision": "c64c1210356a104566026ff394d535aa",
+    "url": "/static/media/mglass.c64c1210.svg"
+  },
+  {
+    "revision": "18b858f15334ce26a7070730f13426dd",
+    "url": "/static/media/nocover.18b858f1.png"
+  },
+  {
+    "revision": "ea036f5d979c6355f286c09bb80aa797",
+    "url": "/static/media/process.ea036f5d.svg"
+  }
+]);
