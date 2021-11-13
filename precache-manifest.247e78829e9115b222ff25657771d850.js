08c739472e59e605e850f1fbb889d667a1685145
Arturov Kirill <kaarturov@sberbank.ru>
2021-11-13 20:18:24 +0300

    Updates

---
+++ precache-manifest.247e78829e9115b222ff25657771d850.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94b05957fe38b711c7297d9a1f81cf62",
+    "url": "/RichBee/index.html"
+  },
+  {
+    "revision": "56506ce9f144f9c612a5",
+    "url": "/RichBee/static/css/main.a2689c4e.chunk.css"
+  },
+  {
+    "revision": "07c548e0c971a3e4a1c5",
+    "url": "/RichBee/static/js/2.fefa7ef9.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/RichBee/static/js/2.fefa7ef9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56506ce9f144f9c612a5",
+    "url": "/RichBee/static/js/main.8f31481f.chunk.js"
+  },
+  {
+    "revision": "aa93ff99e8d06c362874",
+    "url": "/RichBee/static/js/runtime-main.9ebb09af.js"
+  },
+  {
+    "revision": "f2a01c3dec0eb7365f7e1adba32ae831",
+    "url": "/RichBee/static/media/cross.f2a01c3d.svg"
+  },
+  {
+    "revision": "c64c1210356a104566026ff394d535aa",
+    "url": "/RichBee/static/media/mglass.c64c1210.svg"
+  },
+  {
+    "revision": "18b858f15334ce26a7070730f13426dd",
+    "url": "/RichBee/static/media/nocover.18b858f1.png"
+  },
+  {
+    "revision": "ea036f5d979c6355f286c09bb80aa797",
+    "url": "/RichBee/static/media/process.ea036f5d.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.85b8d722b9f2d678850f4ce0dae1e02d.js"
+  "/RichBee/precache-manifest.247e78829e9115b222ff25657771d850.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/RichBee/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
